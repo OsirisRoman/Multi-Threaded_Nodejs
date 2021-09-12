e988a2d9a009eb23b8349bf4ebc388acbb57e932
Osiris Roman <osiris.roman@ulinksolution.com>
2021-09-11 22:26:18 -0500

    Create one server-child when executed

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
 
-function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {}
+// Is the file being executed in master mode?
+if(cluster.isMaster) {
+    // Cause index.js to be executed *again* but
+    // in child mode
+    cluster.fork();
+} else {
+    // I'm a child, Im going to act like a server
+    // and do nothing else
+    const express = require("express");
+    const app = express();
+    
+    function doWork(duration) {
+        const start = Date.now();
+        while(Date.now() - start < duration) {}
+    }
+    
+    app.get("/", (req, res) => {
+        doWork(5000);
+        res.send("Hi There! Ready for Nodejs in Multi-Thread Mode!?");
+    })
+    
+    const port = 3000;
+    app.listen(port, () => {
+        console.log(`server runing on port ${port}`);
+    })
 }
-
-app.get("/", (req, res) => {
-    doWork(5000);
-    res.send("Hi There! Ready for Nodejs in Multi-Thread Mode!?");
-})
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`server runing on port ${port}`);
-})
